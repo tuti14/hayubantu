aba0083ce9286288120b24584493c57beb7a3432
tuti14 <rahmawatituti820@gmail.com>
2021-05-25 21:48:16 +0700

    menambahkan function galangdana untuk halaman sebelum login di fitur galang dana

--- application/controllers/Fitur/View_fitur.php
+++ application/controllers/Fitur/View_fitur.php
@@ -22,14 +22,34 @@ class View_fitur extends CI_Controller {
 		$this->load->view('templates/footer_home');
 	}
 
-	public function inbox() {
+	public function update() {
 
 		$this->load->view('templates/header_home');
 		$this->load->view('templates/header');
-		$this->load->view('fitur/fitur_inbox/hal1_fitur_inbox');
+		$this->load->view('fitur/fitur_inbox/hal1_fitur_update_inbox');
 		$this->load->view('templates/footer');
 		$this->load->view('templates/footer_home');
 	}
+
+	public function notification() {
+
+		$this->load->view('templates/header_home');
+		$this->load->view('templates/header');
+		$this->load->view('fitur/fitur_inbox/hal1_fitur_notifikasi_inbox');
+		$this->load->view('templates/footer');
+		$this->load->view('templates/footer_home');
+	}
+
+	public function galangdana() {
+
+		$this->load->view('templates/header_home');
+		$this->load->view('templates/header');
+		$this->load->view('fitur/fitur_galangdana/hal1_fitur_galangdana');
+		$this->load->view('templates/footer');
+		$this->load->view('templates/footer_home');
+	}
+
+
 }
 
 
