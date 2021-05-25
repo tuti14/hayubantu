d9ca89d5efc16f65d62f0cc39565d2832814bd09
tuti14 <rahmawatituti820@gmail.com>
2021-05-25 21:50:21 +0700

    mengedit file controller home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,10 +6,10 @@ class Home extends CI_Controller {
 
 	public function index () {
 
-
 		$this->load->view('templates/header_home');
 		$this->load->view('home');
 		$this->load->view('templates/footer_home');
+		$this->load->view('templates/footer');
 		
 		
 	}
